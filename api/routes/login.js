16f30c49ac518e29d27eb03f1f4cc837a6f9a21f
ghaneeyat09 <69092604+ghaneeyat09@users.noreply.github.com>
2020-12-21 16:10:49 +0100

    ppp

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/userReg');
 const { generateToken, authorizeUser } = require('../../auth/auth');
 require("dotenv").config();
--- api/routes/register.js
+++ api/routes/register.js
@@ -1,7 +1,7 @@
 const {generateToken, authorizeUser} = require('../../auth/auth');
 const express = require('express');
 const User = require('../models/userReg');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 
