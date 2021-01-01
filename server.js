51497852d739bd5cd0ad322a54c51314975efce1
ghaneeyat09 <69092604+ghaneeyat09@users.noreply.github.com>
2021-01-01 21:16:48 +0100

    new commit

--- server.js
+++
@@ -1,9 +0,0 @@
-/*const http = require('http');
-const app = require('./app');
-const PORT = process.env.PORT || 8080;
-const server = http.createServer(app);
-
-
-server.listen(PORT, () => {
-    console.log('now listening to port 5050');
-});*/
