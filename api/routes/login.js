e36a2fc4186fef1880ee31927d423804e61ba66d
ghaneeyat09 <69092604+ghaneeyat09@users.noreply.github.com>
2021-01-16 21:16:11 +0100

    mmm

--- api/routes/login.js
+++ api/routes/login.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const User = require('../models/userReg');
+const Admin = require('../models/admin');
 const { generateToken, authorizeUser } = require('../../auth/auth');
 require("dotenv").config();
 
@@ -53,7 +54,51 @@ router.post('/login', (req, res) => {
             })
         })
 });
+router.post('/login/admin', (req, res) => {
+    Admin.find({email: req.body.email})
+    .then((admin) => {
+        if(admin.lenght < 1){
+            res.status(401).json({
+                message: 'user not found'
+            });
+        }
+           bcrypt.compare(req.body.password, admin[0].password, (err, result) => {
+        if(err) {
+            return res.status(401).json({
+                error: err
+                   }) 
+                }
+        if(result) {
+            console.log(result);
+            generateToken(admin[0], (err, token) => {
+                if(err){
+                 console.log("error", err);
+                }
+                else{
+                  console.log(token);
+                  res.status(201).json({
+                  message: "Auth successful",
+                  admin: admin[0],
+                  token: token
+                    })
+                }
+    });
+}
 
+        else{
+            res.status(401).json({
+                    message: "incorrect password"
+                }) 
+            }   
+        }
+            )
+        })
+        .catch((err) => {
+            res.status(404).json({
+                message: 'admin not found'
+            })
+        })
+});
 router.get("/login/:id", authorizeUser, (req, res) => {
         User.findById({_id: req.params.id})
         .exec()
@@ -70,5 +115,20 @@ router.get("/login/:id", authorizeUser, (req, res) => {
             })
         })
 })
-
+router.get("/login/:id/admin", authorizeUser, (req, res) => {
+    Admin.findById({_id: req.params.id})
+    .exec()
+    .then((data) => {
+        res.status(200).json({
+            success: true,
+            data
+        })
+    })
+    .catch((err) => {
+        res.json({
+            message: "user not found",
+            error: err
+        })
+    })
+})
 module.exports = router;
